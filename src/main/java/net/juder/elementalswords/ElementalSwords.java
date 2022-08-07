ad05916133363f96770d4b2900cf0018edaa03cf
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2022-08-06 21:15:38 -0700

    first commit

---
+++ src/main/java/net/juder/elementalswords/ElementalSwords.java
@@ -0,0 +1,10 @@
+package net.juder.elementalswords;
+
+import org.slf4j.Logger;
+
+public class ElementalSwords {
+    
+    public static final String MOD_ID = "elementalswords";
+
+    public static final 
+}
---
+++ src/main/java/net/juder/elementalswords/item/ModItems.java
@@ -0,0 +1,5 @@
+package net.juder.elementalswords.item;
+
+public class ModItems {
+    
+}
